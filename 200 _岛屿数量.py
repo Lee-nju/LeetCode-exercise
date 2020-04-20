aaf6454c349b1fcf79b756e995b1b5463c764bec
Lee-nju <790955169@qq.com>
2020-04-21 00:26:19 +0800

    200. 岛屿数量 空间优化

--- 200 _岛屿数量.py
+++ 200 _岛屿数量.py
@@ -12,6 +12,7 @@ def numIslands(grid: [[str]]) -> int:
 
     row = len(grid)
     col = len(grid[0])
+    # 访问过后的可以重置为0 不用另存数组
     visited = [[False for j in range(col)] for i in range(row)]
     priorityQueue = deque()
     res = 0
@@ -29,12 +30,54 @@ def numIslands(grid: [[str]]) -> int:
                     if index[1] > 0 and not visited[index[0]][index[1] - 1] and grid[index[0]][index[1] - 1] != '0':
                         priorityQueue.append([index[0], index[1] - 1])
                         visited[index[0]][index[1] - 1] = True
-                    if index[0] < row - 1 and not visited[index[0] + 1][index[1]] and grid[index[0] + 1][index[1]] != '0':
+                    if index[0] < row - 1 and not visited[index[0] + 1][index[1]] and grid[index[0] + 1][
+                        index[1]] != '0':
                         priorityQueue.append([index[0] + 1, index[1]])
                         visited[index[0] + 1][index[1]] = True
-                    if index[1] < col - 1 and not visited[index[0]][index[1] + 1] and grid[index[0]][index[1] + 1] != '0':
+                    if index[1] < col - 1 and not visited[index[0]][index[1] + 1] and grid[index[0]][
+                        index[1] + 1] != '0':
                         priorityQueue.append([index[0], index[1] + 1])
                         visited[index[0]][index[1] + 1] = True
+
+    return res
+
+
+def numIslands2(grid: [[str]]) -> int:
+    """
+    广度优先遍历, 空间上优化上述方法
+
+    pass
+    """
+    if not grid:
+        return 0
+
+    row = len(grid)
+    col = len(grid[0])
+    # 访问过后的可以重置为0 不用另存数组
+    # visited = [[False for j in range(col)] for i in range(row)]
+    priorityQueue = deque()
+    res = 0
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] != '0':
+                res = res + 1
+                grid[i][j] = '0'
+                priorityQueue.append([i, j])
+                while priorityQueue:
+                    index = priorityQueue.popleft()
+                    if index[0] > 0 and grid[index[0] - 1][index[1]] != '0':
+                        priorityQueue.append([index[0] - 1, index[1]])
+                        grid[index[0] - 1][index[1]] = '0'
+                    if index[1] > 0 and grid[index[0]][index[1] - 1] != '0':
+                        priorityQueue.append([index[0], index[1] - 1])
+                        grid[index[0]][index[1] - 1] = '0'
+                    if index[0] < row - 1 and grid[index[0] + 1][index[1]] != '0':
+                        priorityQueue.append([index[0] + 1, index[1]])
+                        grid[index[0] + 1][index[1]] = '0'
+                    if index[1] < col - 1 and grid[index[0]][
+                        index[1] + 1] != '0':
+                        priorityQueue.append([index[0], index[1] + 1])
+                        grid[index[0]][index[1] + 1] = '0'
 
     return res
 
@@ -43,3 +86,4 @@ test1 = [[1, 1, 1, 1, 0], [1, 1, 0, 1, 0], [1, 1, 0, 0, 0], [0, 0, 0, 0, 0]]
 test2 = [[1, 1, 0, 0, 0], [1, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 1]]
 test2 = [[str(test2[i][j]) for j in range(5)] for i in range(4)]
 print(numIslands(test2))
+print(numIslands2(test2))
