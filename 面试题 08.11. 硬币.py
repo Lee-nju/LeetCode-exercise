61351e61666f05c631eb4de0c2af9e0c0bd920a0
Lee-nju <790955169@qq.com>
2020-04-24 21:29:04 +0800

    面试题 08.11. 硬币

---
+++ 面试题 08.11. 硬币.py
@@ -0,0 +1,22 @@
+mod = 1000000007
+
+
+def waysToChange(n: int) -> int:
+    if n <= 0:
+        return 0
+    if n < 5:
+        return 1
+
+    res = 0
+    for i in range(n // 25 + 1):
+        rest = n - 25 * i
+        a0 = rest // 10
+        b0 = (rest % 10) // 5
+        res = res + (a0 + b0 + 1) * (a0 + 1) % mod
+        res = res % mod
+
+    return res
+
+
+# 输入61 预期73
+print(waysToChange(61))
