88895df8d3d6e93bb3f1c47155dbd2ac34b97753
Lee-nju <790955169@qq.com>
2020-05-05 12:32:36 +0800

    LeetCode TreeNode

---
+++ LeetCode/TreeNode.py
@@ -0,0 +1,59 @@
+from _collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def list2Tree(l: [int]):
+    """
+    将list=[5,1,4,null,null,3,6] 转成树节点的格式
+
+    :param l:
+    :return:
+    """
+    root = TreeNode(l[0])
+    i, queue = 1, deque()
+    queue.append(root)
+    while queue:
+        k = len(queue)
+        for j in range(k):
+            node = queue.popleft()
+            if i == len(l):
+                continue
+            if l[i] != 'null':
+                node.left = TreeNode(l[i])
+                queue.append(node.left)
+            i = i + 1
+            if l[i] != 'null':
+                node.right = TreeNode(l[i])
+                queue.append(node.right)
+            i = i + 1
+
+    return root
+
+
+def print_tree(root):
+    """层次遍历输出"""
+    if not root:
+        return
+    queue = deque()
+    queue.append(root)
+    m_list = []
+    while queue:
+        node = queue.popleft()
+        m_list.append(node.val)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append((node.right))
+
+    print(m_list)
+
+
+m_tree = list2Tree([5, 1, 4, 'null', 'null', 3, 6, 7, 'null', 1, 'null', 'null', 'null', 'null', 'null'])
+print_tree(m_tree)
