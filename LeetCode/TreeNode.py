21459b9f0a680630ce67325e39f176f8dc48dfb6
Lee-nju <790955169@qq.com>
2020-05-05 12:45:32 +0800

    LeetCode TreeNode

--- LeetCode/TreeNode.py
+++ LeetCode/TreeNode.py
@@ -9,51 +9,54 @@ class TreeNode:
         self.right = None
 
 
-def list2Tree(l: [int]):
-    """
-    将list=[5,1,4,null,null,3,6] 转成树节点的格式
+class Tree:
+    def __init__(self):
+        self.root = TreeNode(-1)
 
-    :param l:
-    :return:
-    """
-    root = TreeNode(l[0])
-    i, queue = 1, deque()
-    queue.append(root)
-    while queue:
-        k = len(queue)
-        for j in range(k):
+    def list2Tree(self, l: [int]):
+        """
+        将list=[5,1,4,null,null,3,6] 转成树节点的格式
+
+        :param l:
+        :return:
+        """
+        self.root = TreeNode(l[0])
+        i, queue = 1, deque()
+        queue.append(self.root)
+        while queue:
+            k = len(queue)
+            for j in range(k):
+                node = queue.popleft()
+                if i == len(l):
+                    continue
+                if l[i] != 'null':
+                    node.left = TreeNode(l[i])
+                    queue.append(node.left)
+                i = i + 1
+                if l[i] != 'null':
+                    node.right = TreeNode(l[i])
+                    queue.append(node.right)
+                i = i + 1
+        return self
+
+    def print_tree(self):
+        """层次遍历输出"""
+        if self.root.val == -1:
+            return
+        queue = deque()
+        queue.append(self.root)
+        m_list = []
+        while queue:
             node = queue.popleft()
-            if i == len(l):
-                continue
-            if l[i] != 'null':
-                node.left = TreeNode(l[i])
+            m_list.append(node.val)
+            if node.left:
                 queue.append(node.left)
-            i = i + 1
-            if l[i] != 'null':
-                node.right = TreeNode(l[i])
-                queue.append(node.right)
-            i = i + 1
+            if node.right:
+                queue.append((node.right))
 
-    return root
+        print(m_list)
 
 
-def print_tree(root):
-    """层次遍历输出"""
-    if not root:
-        return
-    queue = deque()
-    queue.append(root)
-    m_list = []
-    while queue:
-        node = queue.popleft()
-        m_list.append(node.val)
-        if node.left:
-            queue.append(node.left)
-        if node.right:
-            queue.append((node.right))
-
-    print(m_list)
-
-
-m_tree = list2Tree([5, 1, 4, 'null', 'null', 3, 6, 7, 'null', 1, 'null', 'null', 'null', 'null', 'null'])
-print_tree(m_tree)
+l = [5, 1, 4, 'null', 'null', 3, 6, 7, 'null', 1, 'null', 'null', 'null', 'null', 'null']
+m_tree = Tree().list2Tree(l)
+m_tree.print_tree()
