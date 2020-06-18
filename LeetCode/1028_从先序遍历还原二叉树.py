9cad9c34837b11e510e5e5fe2fac640071e6b629
Lee-nju <790955169@qq.com>
2020-06-18 14:43:33 +0800

    1028. 从先序遍历还原二叉树

--- LeetCode/1028_从先序遍历还原二叉树.py
+++ LeetCode/1028_从先序遍历还原二叉树.py
@@ -3,6 +3,13 @@ from collections import deque
 
 
 def transform(S: str, index: int):
+    """
+    当前节点的值
+
+    :param S:
+    :param index:
+    :return:
+    """
     num = 0
     while index < len(S) and '0' <= S[index] <= '9':
         num = num * 10 + int(S[index])
@@ -12,6 +19,13 @@ def transform(S: str, index: int):
 
 
 def depth(S: str, index: int):
+    """
+    当前节点的深度
+
+    :param S:
+    :param index:
+    :return:
+    """
     dep = 0
     while index < len(S) and S[index] == '-':
         dep = dep + 1
@@ -22,21 +36,32 @@ def depth(S: str, index: int):
 
 class Solution:
     def recoverFromPreorder(self, S: str) -> TreeNode:
+        # 根节点的值
         val, begin = transform(S, 0)
+        # 建树
         root = TreeNode(val)
         stack = deque()
         stack.append(root)
+        # 循环至字符串结尾
         while begin < len(S):
+            # 当前节点的深度，并更新下标
             dep, begin = depth(S, begin)
+            # 当前节点的值，并更新下标
             val, begin = transform(S, begin)
+            # 如果深度小于栈里的元素个数，出栈至深度等于栈里的元素个数
             if dep < len(stack):
                 while dep < len(stack):
                     stack.pop()
 
                 node = TreeNode(val)
+                # 将其加到栈顶的右子节点上
                 stack[-1].right = node
+                # 入栈
                 stack.append(node)
+
+            # 如果深度等于栈里的元素个数
             else:
+                # 临时节点加到栈顶的左子节点上，并入栈
                 node = TreeNode(val)
                 stack[-1].left = node
                 stack.append(node)
